3ca371c392a5e7a9d81680ba8ce239218eea5f50
tkerroum <tkerroum@c4r7p3.1337.ma>
2023-12-15 05:20:48 +0100

    -m

--- ft_printu.c
+++ ft_printu.c
@@ -15,10 +15,10 @@
 int	ft_printu(unsigned int num)
 {
 	unsigned int	counter;
+	long			numb;
 
+	numb = num;
 	counter = 0;
-	if (num == 4294967295)
-		counter += ft_putstr("4294967295");
 	if (num >= 10)
 		counter += ft_putnbr(num / 10);
 	if (num >= 0 && num <= 9)
